391af032235831c81ae22a21b24ed58ee9b5523d
Jhoneyrrosa <jhoneyrrosagmail.com>
2024-08-06 13:26:34 -0300

    First commit

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+let idade = 87
+
+
+// array e objetos
+let ingredientes = ["Agua", "Farinha", "leite", "Açucar"]
+
+let personagem = {
+    nome: "jhoney",
+    esforco: 10,
+    medo: 1,
+    combate: 80095700000
+}
+
+console.log(personagem.esforco)
