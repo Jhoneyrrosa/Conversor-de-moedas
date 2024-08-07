9d31dd01e05296d5221444bae5ddc75e51dbee57
Jhoneyrrosa <jhoneyrrosagmail.com>
2024-08-07 09:11:26 -0300

    array e objeto

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
 let idade = 87
 
 
-// array e objetos
+// array e... 
 let ingredientes = ["Agua", "Farinha", "leite", "Açucar"]
 
+// ...objeto
 let personagem = {
     nome: "jhoney",
     esforco: 10,
@@ -11,4 +12,12 @@ let personagem = {
     combate: 80095700000
 }
 
-console.log(personagem.esforco)
+function somar(a, b) {
+    let resultado = a + b
+return resultado
+}
+
+let x = somar(15, 5)
+let y = somar(2, 8)
+
+console.log(x, y)
