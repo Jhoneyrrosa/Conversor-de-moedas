df39d1cbe0d183916580f1740e79b080211c4093
Jhoneyrrosa <jhoneyrrosagmail.com>
2024-08-09 09:06:16 -0300

    Funcion e IF ElSE

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,18 @@
-let idade = 87
+let idade = 87  // Tipo Number.
+let nome = "Jhoney R.R."  // Tipo "string".
+let logado = false  //Tipo Boolean.
 
 
 // array e... 
-let ingredientes = ["Agua", "Farinha", "leite", "Açucar"]
+/* let ingredientes = ["Agua", "Farinha", "leite", "Açucar"] // array dentro do conchetese com virgula.
 
 // ...objeto
-let personagem = {
-    nome: "jhoney",
+let personagem = { 
+    nome: "jhoney Rosa",
     esforco: 10,
     medo: 1,
     combate: 80095700000
-}
+}  // objeto. dados de um personagem.
 
 function somar(a, b) {
     let resultado = a + b
@@ -20,4 +22,10 @@ return resultado
 let x = somar(15, 5)
 let y = somar(2, 8)
 
-console.log(x, y)
+console.log(x, y) */
+
+if ( nome == "Jhoney R. R.") {
+    console.log ("Olá, Sr. Jhoney!")
+} else {
+    console.log("Não cadastrado!")
+}
